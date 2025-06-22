3682d9a9addc1fe5473ce6ab506038dda25cf33f
Saifansari-ai <saifansari300b@gmail.com>
2025-06-22 09:14:07 +0530

    Signup and signin done

--- oops_proj.py
+++ oops_proj.py
@@ -16,9 +16,9 @@ class chatbook:
           """)
     
         if user_input == "1":
-             pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -26,6 +26,29 @@ class chatbook:
         else:
             exit()
  
+    def signup(self):
+        user = input("enter your email :")
+        pwd = input("Create a password :")
+        self.username = user
+        self.password = pwd
+        print("You have signup successfully!!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            user = input("enter your email/username here ->")
+            pwd = input("Enter your password here ->")
+            if self.username == user and self.password == pwd:
+                print("You have logged in successfully!!")
+                self.loggedin = True
+            else:
+                print("Invalid username or password")
+        print("\n")
+        self.menu()
+
 
     
 chat = chatbook()
